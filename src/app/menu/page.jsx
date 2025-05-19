37721d682c99cfe13dff98c744bd2c0ebd170e6e
yakoltsevich <tpaktap1221@gmail.com>
2025-05-20 00:50:54 +0200

    asd

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -28,7 +28,7 @@ export default function Menu() {
     };
 
     return (
-        <div className="max-w-7xl mx-auto p-4 min-h-[calc(100vh-217px)]  bg-lightColor space-y-2">
+        <div className="max-w-7xl mx-auto p-4 min-h-[calc(100vh-217px)] space-y-2">
             {error && <p className="text-red-500">{error}</p>}
             <div className='flex-1' />
             {
