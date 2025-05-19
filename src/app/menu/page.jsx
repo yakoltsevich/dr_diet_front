6e7b37fd09daf486b1cd6b7bfdfd02f6c77ebf48
yakoltsevich <tpaktap1221@gmail.com>
2025-05-20 00:43:28 +0200

    asd

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -8,9 +8,6 @@ import { MOCKED_MENU } from "@/shared/constants";
 
 export default function Menu() {
     const [menu, setMenu] = useState(null);
-    const [loading1, setLoading1] = useState(false);
-    const [loading2, setLoading2] = useState(false);
-    const [loading3, setLoading3] = useState(false);
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -30,65 +27,8 @@ export default function Menu() {
         }
     };
 
-    const generateMenu = async () => {
-        setLoading1(true);
-        setError(null);
-        try {
-            const response = await axiosClient.post('/menu/generate', { body: {} });
-            setMenu(response.data.menu);
-        } catch (err) {
-            console.error(err);
-            setError('Error while generating the menu');
-        } finally {
-            setLoading1(false);
-        }
-    };
-    const fillRecipe = async () => {
-        setLoading2(true);
-        setError(null);
-        try {
-            const response = await axiosClient.post('/menu/fill-recipe', { body: {} });
-            setMenu(response.data.menu);
-        } catch (err) {
-            console.error(err);
-            setError('Error while generating the menu');
-        } finally {
-            setLoading2(false);
-        }
-    };
-
-    const calculateNutrition = async () => {
-        setLoading3(true);
-        setError(null);
-        try {
-            const response = await axiosClient.post('/menu/calculate-nutrition', { body: {} });
-            setMenu(response.data.menu);
-        } catch (err) {
-            console.error(err);
-            setError('Error while generating the menu');
-        } finally {
-            setLoading3(false);
-        }
-    };
-
     return (
-        <div className="max-w-7xl mx-auto p-4 min-h-[calc(100vh-217px)] space-y-2">
-            {
-                 (
-                    <ButtonGroup size={'sm'} className={'w-full sm:max-w-xs '}>
-                        <Button onPress={generateMenu} disabled={loading1} className="w-xs">
-                            {loading1 ? 'Generating menu...' : 'Generate menu'}
-                        </Button>
-                        <Button onPress={fillRecipe} disabled={loading2} className="w-xs">
-                            {loading2 ? 'filling Recipe...' : 'Fill Recipe'}
-                        </Button>
-                        <Button onPress={calculateNutrition} disabled={loading3} className="w-xs">
-                            {loading3 ? 'calculating...' : 'Calculate Nutrition'}
-                        </Button>
-                    </ButtonGroup>
-                )
-            }
-
+        <div className="max-w-7xl mx-auto p-4 min-h-[calc(100vh-217px)]  bg-lightColor space-y-2">
             {error && <p className="text-red-500">{error}</p>}
             <div className='flex-1' />
             {
