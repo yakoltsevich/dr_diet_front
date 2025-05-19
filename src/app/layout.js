7df4aa12463d2403bd49e16acf71ee65fec1f099
yakoltsevich <tpaktap1221@gmail.com>
2025-05-20 00:29:04 +0200

    asd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,25 @@
-// src/app/layout.js
 import './globals.css';
 import { Providers } from './providers';
 import { Manrope } from 'next/font/google';
 import { ClientLayoutShell } from './client-layout-shell';
 
-
 export const metadata = {
     title: 'Dr Diet App',
     description: 'Menu and Food Diary',
     manifest: '/manifest.json',
-    themeColor: '#5e7a76',
     icons: {
         icon: '/icons/icon-192.png',
         shortcut: '/icons/icon-192.png',
         apple: '/icons/icon-192.png',
     },
 };
+
+export const viewport = {
+    themeColor: '#5e7a76',
+    width: 'device-width',
+    initialScale: 1,
+};
+
 const manrope = Manrope({
     subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext'],
 });
@@ -24,9 +28,6 @@ export default function RootLayout({ children }) {
     return (
         <html lang="ru">
         <head />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#5e7a76" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <body className={manrope.className}>
         <Providers>
             <ClientLayoutShell>{children}</ClientLayoutShell>
